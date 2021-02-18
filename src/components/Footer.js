34e1bd030e3cbacb10bedd677c692a77f9692f4e
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-02-18 20:55:46 +0400

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,41 +5,29 @@ import Logo from '../assets/logo.png';
 
 function Footer() {
     return (
-        <div className="container">
-            <div className="footer">
+        <div className="footer">
 
-                <div className="footer__left">
-                    <img className="footer__logo" src={Logo} alt="" />
-                </div>
-
-                
-
-                <div className="footer__right">
-                    <div className="footer-menu">
-                        <Link to="/contacts">
-                            <span>Контакты</span>
-                        </Link>
-
-                        <Link to="/privacy-policy">
-                            <span>Политика конфиденциальности</span>
-                        </Link>
-
-                        <Link to="/terms">
-                            <span>Условия предоставления услуг</span>
-                        </Link>
-                    </div>
-                </div>
-               
-                
-                
-                
-
-
-
-            </div>
-
-
+        <div className="footer__left">
+            <img className="footer__logo" src={Logo} alt="" />
         </div>
+
+        <div className="footer__right">
+            <div className="footer-menu">
+                <Link to="/contacts">
+                    <span>Контакты</span>
+                </Link>
+
+                <Link to="/privacy-policy">
+                    <span>Политика конфиденциальности</span>
+                </Link>
+
+                <Link to="/terms">
+                    <span>Условия предоставления услуг</span>
+                </Link>
+            </div>
+        </div>
+          
+    </div>
                 
     )
 }
